c726bd343bb571af860427215fab362320707ab3
Alish705 <alishawal705@gmail.com>
2020-11-30 12:51:56 +1100

    Use of cookies and sessions

--- app.js
+++ app.js
@@ -1,82 +1,108 @@
 //jshint esversion:6
-require("dotenv").config();  ///package for environment variable dotenv
+require("dotenv").config(); ///package for environment variable dotenv
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt = require("mongoose-encryption");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport"); ////Require 3 package
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true}, {useUnifiedTopology: true});
+app.use(session({
+  secret: "We all have our own secret.", /// express-session code
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize()); /// passport code
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-///////////Moongoose encryption with specific encrypted fields////////////////////
-
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 
-app.get("/", function(req,res){
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser()); /// passport_local_mongoose code
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req,res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req,res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
+app.get("/secrets", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
 
-app.post("/register", function(req,res){
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password:hash
-    });
-
-    newUser.save(function(err){
-      if(err){
-        console.log(err);
-      }else{
-        res.render("secrets");
-      }
-    });
-  });
-
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/");
 });
 
 
+app.post("/register", function(req, res) {
+  User.register({
+    username: req.body.username
+  }, req.body.password, function(err, user) {
+    if (err) {
+      console.log(err); //passport local mongoose
+      res.redirect("/register");
+    } else {
+      // authentication usein passport. Local is type of authentication
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+    }
+  });
 
-app.post("/login", function(req,res){
-  const username = req.body.username;
-  const password = req.body.password;
+});
+app.post("/login", function(req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  ////passport function login
 
-  User.findOne({email:username}, function(err,foundUser){
-    if(err){
+  req.login(user, function(err) {
+    if (err) {
       console.log(err);
-    }else{
-      if(foundUser){
-          bcrypt.compare(password, foundUser.password, function(err, result) {
-            if(result === true){
-              res.render("secrets");
-            }
-          });
-        }
-      }
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        res.redirect("/secrets");
+      });
+    }
   });
 });
 
@@ -84,7 +110,6 @@ app.post("/login", function(req,res){
 
 
 
-
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
